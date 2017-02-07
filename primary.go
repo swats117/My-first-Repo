31bddef3b2db5480a2be95cf99e92bb52349d43d
swarts2 <swarts2@illinois.edu>
2017-02-06 19:59:49 -0600

    commit for pushing to origin?

--- primary.go
+++ primary.go
@@ -6,7 +6,7 @@ import(
 // This is an experimental comment.
 // Vectorized linearization
 
-
+//https://github.com/swats117/My-first-Repo.git
 func main(){
 	fmt.Printf("Oh, wait. I wanted to change something!")
 	fmt.Printf("I am a go program.\n")
